a2fa9bf9e3c993e829c08193d203d517028fd3ef
Dan Reeves <danreeves125@hotmail.com>
2022-07-21 07:33:44 +0100

    bug fixed

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route Files
-const bootcamps = require('./routes/bootcamp');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
+// Route Files
+const bootcamps = require('./routes/bootcamp');
+
+// connect to DB
+connectDB();
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev Logging Middleware
 if(process.env.NODE_ENV === 'development') {
@@ -20,6 +28,18 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} and listening on port ${PORT}!`);
+const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} and listening on port ${PORT}!` .yellow.bold);
+});
+
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    // Close server
+    server.close(() => {
+        process.exit(1);
+    });
 });
